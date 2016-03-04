543414e511d4fce00f40b75ea510e7ccd0d75a7f
Philip Lan <philiplan2017@u.northwestern.edu>
2016-03-04 03:35:13 -0600

    finishing ingredients'

--- Team10/ingredients/ingredients.py
+++ Team10/ingredients/ingredients.py
@@ -1,15 +1,15 @@
 import json
 import nltk
+import string
 
+### loading measurements ###
 with open('units.json') as data_file:
 	units_measure = set(json.load(data_file))
 
 def parse_ingredients(entry):
-	### loading measurements ###
-	
-
 	output = {}
 	entry_list = entry.split()
+
 	### finding quantity 	 ###
 	quantity = ["0","1",None]
 	for j in entry_list[0]:
@@ -28,13 +28,32 @@ def parse_ingredients(entry):
 
 	### finding name 		 ###
 	name = []
+
+	### Only 1 word left in list ###
+	if len(entry_list) == 1:
+		word = nltk.pos_tag(entry_list,tagset="universal")
+		if word[0][1] == "NOUN":
+			name = [word[0][0]]
+
+	### Create Bigrams and find parts of speech that are nouns ###
 	for bigram in nltk.bigrams(entry_list):
 		tags = nltk.pos_tag(bigram,tagset="universal")
-		for entry in tags:
-			### THIS IS MESSED UP FIX IT BRO ####
-			if entry[1] == "NOUN": name.append(entry[0])
-				  
-			
+		check_bigrams = []
+		for i in tags:
+			if i[1] == "NOUN":
+				check_bigrams.append(i[0])
+		if len(check_bigrams) == 2:
+			name.append(string.join(check_bigrams))
+		else:
+			if check_bigrams:
+				if check_bigrams[0] not in name:
+					name.append(check_bigrams[0])
+		### Unigram ###
+		for i in bigram:
+			word = nltk.pos_tag([i],tagset="universal")
+			if word[0][1] == "NOUN" and word[0][0] not in name:
+				name.append(word[0][0])
+
 	### finding descriptor 	 ###
 	output["entry"] = entry
 	output["name"] = name
