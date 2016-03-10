cc8d6812b0d6973b3e72385991c04f62d58027bd
hspindell <henryspindell@gmail.com>
2016-03-10 15:02:29 -0600

    added title to scraper

--- Team10/scraper.py
+++ Team10/scraper.py
@@ -6,7 +6,8 @@ def get_recipe(url):
     r = urllib.urlopen(url).read()
     soup = BeautifulSoup(r)
 
-    o = {'ingredients':[],'directions':[]}
+    o = {'title':'','ingredients':[],'directions':[]}
+    o['title'] = soup.find("h1", {"itemprop":"name"}).text
     o['ingredients'] = [ingredient.text for ingredient in soup.findAll("span", {"itemprop":"ingredients"})]
     o['directions'] = [direction.text for direction in soup.findAll("span",{"class":"recipe-directions__list--item"}) if len(direction) > 0]
     
