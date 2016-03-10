321fb537a448c19cd37bca1194525fb1b69b07ad
hspindell <henryspindell@gmail.com>
2016-03-10 12:25:07 -0600

    improvements to tools

--- Team10/test.py
+++ Team10/test.py
@@ -1,7 +1,7 @@
 from scraper import get_recipe
 from tools import tools
 
-url = "http://allrecipes.com/recipe/13566/cranberry-nut-bread-i/"
+url = "http://allrecipes.com/Recipe/Baked-Lemon-Chicken-with-Mushroom-Sauce/"
 recipe = get_recipe(url)
 
 tools.find_tools(recipe['directions'])
--- Team10/tools/tools.py
+++ Team10/tools/tools.py
@@ -1,6 +1,13 @@
 import json
 import re
 TOOLS = []
+APPLIANCES = ["oven","microwave","microwave oven","stove","toaster","oven broiler","broiler"]
+
+# removing duplicates and preserving order  http://stackoverflow.com/questions/480214/how-do-you-remove-duplicates-from-a-list-in-python-whilst-preserving-order
+def f7(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
 
 def load_tools():
     global TOOLS
@@ -12,16 +19,24 @@ def load_tools():
 def find_tools(steps):
     tools_by_step = []
     all_tools = []
+    
+    # TODO mappings like basting-> requires baster, mix->requires spoon/fork/mixer, "turning + oven" => tongs
+    # slice => knife
+    # or if sliced or chopped are in ingredients
 
+    # TODO go through all steps and if any step tool is believed to be from a previous step, remove it
+    # from all_tools
     for (i,s) in enumerate(steps):
         step_tools = tools_for_step(s)
         tools_by_step.append(step_tools)
         
         # if it's preceded by "the" it's probably referring to a tool introduced earlier
+        # TODO are all newly introduced tools preceded directly by "a"? might add this
         for st in step_tools:            
-            if not re.findall(r"the \w* " + st, s):
+            if st in APPLIANCES or not re.findall(r"the\s*\w*\s*" + st, s):
                 all_tools.append(st)
     
+    all_tools = f7(all_tools)
     print tools_by_step
     print all_tools
     
@@ -44,7 +59,10 @@ def tools_for_step(step):
     for rt1 in recipe_tools:
         unique = True
         for rt2 in recipe_tools:
-            if rt1 in rt2 and rt1 != rt2 and len(rt1.split()) < len(rt2.split()):
+            a1 = rt1 in APPLIANCES
+            a2 = rt2 in APPLIANCES
+            
+            if a1 == a2 and rt1 in rt2 and rt1 != rt2 and len(rt1.split()) < len(rt2.split()):
                 unique = False
         if unique:
             tools.append(rt1)
