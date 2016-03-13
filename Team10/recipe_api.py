5a76c9a89103aacf38e979883df63ffca0e555af
hspindell <henryspindell@gmail.com>
2016-03-13 18:43:24 -0500

    parse_recipe for web purposes

--- Team10/recipe_api.py
+++ Team10/recipe_api.py
@@ -6,13 +6,42 @@ from tools import tools
 import scraper
 from pprint import pprint
 
-preptools = {'fork':['mixed'],'knife':['chopped','sliced','minced','diced'], 'grater':['grated'], 'mortar and pestle':['crushed']}
-methodtools = {'baster':['basting'], 'oven':['bake','broil'], 'fork':['mix']}
+PREPTOOLS = {'fork':['mixed'],'knife':['chopped','sliced','minced','diced'], 'grater':['grated'], 'mortar and pestle':['crushed']}
+METHODTOOLS = {'baster':['basting'], 'oven':['bake','broil'], 'fork':['mix']}
 
 def remove_duplicates(seq):
     seen = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
+    
+def recipe_by_steps(directions, ingredients, methods_by_step, tools_by_step): 
+    steps = []
+    for (i, d) in enumerate(directions):
+        step = {}
+        step_methods = methods_by_step[i]
+        step_tools = tools_by_step[i]
+        
+        for sm in step_methods:
+            for t in METHODTOOLS:
+                if sm in METHODTOOLS[t] and t not in step_tools:
+                    tools_by_step[i].append(t)
+                    
+                    
+        step_ingredients = []
+        for ingredient in ingredients:
+            ingredient_words = ingredient['name'].split()
+            for w in ingredient_words:
+                if w in d:
+                    step_ingredients.append(ingredient['name'])
+                    break
+        
+        step['methods'] = methods_by_step[i]
+        step['tools'] = tools_by_step[i]
+        step['ingredients'] = step_ingredients
+        
+        steps.append(step)
+    return steps
+    
 
 def autograder(url):
     '''Accepts the URL for a recipe, and returns a dictionary of the
@@ -35,53 +64,62 @@ def autograder(url):
     tools_by_step, all_tools = tools.find_tools(recipe['directions'])
     results['cooking tools'] = all_tools
 
-
+METHODTOOLS
     for ingredient in results['ingredients']:
-        for t in preptools:
-            if ingredient['preparation'] in preptools[t]:
+        for t in PREPTOOLS:
+            if ingredient['preparation'] in PREPTOOLS[t]:
                 results['cooking tools'].append(t)
 
     for method in results['cooking methods']:
-        for t in methodtools:
-            if method in methodtools[t]:
+        for t in METHODTOOLS:
+            if method in METHODTOOLS[t]:
                 results['cooking tools'].append(t)
 
     results['cooking tools'] = remove_duplicates(results['cooking tools'])
 
-    # steps = recipe_by_steps(recipe['directions'],results['ingredients'],methods_by_step,tools_by_step)
-
-    # pprint(steps)
-    # pprint(results)
     return results
     
     
+
+def parse_recipe(url):
+    '''Accepts the URL for a recipe, and returns a dictionary of the
+    parsed results in the correct format. Also returns recipe divided
+    into steps which each contain methods, tools and ingredients'''
+    results = {}
+    results['url'] = url
+
+    recipe = scraper.get_recipe(url)
+
+    unit_measure, regex = ingredients.load_ingredient_data()
+    results['ingredients'] = []
+    for ing in recipe['ingredients']:
+        results['ingredients'].append(ingredients.parse_ingredients(ing, unit_measure, regex))
+
+    primary_cooking_methods, methods_by_step, cooking_methods = methods.find_all_methods(recipe['title'],recipe['directions'])
+    results['primary cooking method'] = primary_cooking_methods
+    results['cooking methods'] = cooking_methods
+
+    tools_by_step, all_tools = tools.find_tools(recipe['directions'])
+    results['cooking tools'] = all_tools
+
+
+    for ingredient in results['ingredients']:
+        for t in PREPTOOLS:
+            if ingredient['preparation'] in PREPTOOLS[t]:
+                results['cooking tools'].append(t)
+
+    for method in results['cooking methods']:
+        for t in METHODTOOLS:
+            if method in METHODTOOLS[t]:
+                results['cooking tools'].append(t)
+
+    results['cooking tools'] = remove_duplicates(results['cooking tools'])
+    steps = recipe_by_steps(recipe['directions'],results['ingredients'],methods_by_step,tools_by_step)
+    
+    return {'results':results, 'steps':steps}
+
     
     
     
-def recipe_by_steps(directions, ingredients, methods_by_step, tools_by_step): 
-    steps = []
-    for (i, d) in enumerate(directions):
-        step = {}
-        step_methods = methods_by_step[i]
-        step_tools = tools_by_step[i]
-        
-        for sm in step_methods:
-            for t in methodtools:
-                if sm in methodtools[t] and t not in step_tools:
-                    tools_by_step[i].append(t)
-                    
-                    
-        step_ingredients = []
-        for ingredient in ingredients:
-            ingredient_words = ingredient['name'].split()
-            for w in ingredient_words:
-                if w in d:
-                    step_ingredients.append(ingredient['name'])
-                    break
-        
-        step['methods'] = methods_by_step[i]
-        step['tools'] = tools_by_step[i]
-        step['ingredients'] = step_ingredients
-        
-        steps.append(step)
-    return steps
+    
+
