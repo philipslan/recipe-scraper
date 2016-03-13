a2f03dca367750ce739a35441785f1c04b5c4657
hspindell <henryspindell@gmail.com>
2016-03-13 14:03:25 -0500

    generalized transformation classifier

--- Team10/transformations/transformations.py
+++ Team10/transformations/transformations.py
@@ -10,12 +10,28 @@ def load_transformations(category):
     with open(os.path.join(path, category + '.json')) as f:
     	TRANSFORMATIONS[category] = json.load(f)
 
-def is_vegetarian(ingredients):
-    load_transformations('vegetarian')
-    veggie_trans = TRANSFORMATIONS['vegetarian']['trans']
+def is_category(category, ingredients):
     
+    trans_list = []
+    
+    if category == 'vegetarian':
+        load_transformations('vegetarian')
+        trans_list = TRANSFORMATIONS[category]['trans']
+    elif category == 'vegan':
+        load_transformations('vegan')
+        trans_list = TRANSFORMATIONS[category]['trans']
+    elif category == 'low-carb':
+        load_transformations('healthy')
+        trans_list = TRANSFORMATIONS['healthy'][category]
+    elif category == 'low-sodium':
+        load_transformations('healthy')
+        trans_list = TRANSFORMATIONS['healthy'][category]
+    else:
+        print "Category not found"
+            
     for ingredient in ingredients:
-        if ingredient in veggie_trans:
+        if ingredient in trans_list:
             return False
     return True
+        
         
