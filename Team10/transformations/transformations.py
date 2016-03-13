54721a8bce70d3897ac50b309ab2ef9e901c373e
hspindell <henryspindell@gmail.com>
2016-03-13 16:15:01 -0500

    fixed load transformations

--- Team10/transformations/transformations.py
+++ Team10/transformations/transformations.py
@@ -6,7 +6,7 @@ TRANSFORMATIONS = {}
 
 def load_transformations(category):
     global TRANSFORMATIONS
-    if category in TRANSFORMATIONS:
+    if category in TRANSFORMATIONS['to']:
         return
     with open(os.path.join(path, category + '.json')) as f:
     	TRANSFORMATIONS['to'][category] = json.load(f)
@@ -43,16 +43,12 @@ def transform(recipe, category, to_or_from):
     
     trans_list = []
     if category == 'vegetarian':
-        load_transformations(category)
         trans_list = TRANSFORMATIONS[to_or_from][category]['trans']
     elif category == 'vegan':
-        load_transformations(category)
         trans_list = TRANSFORMATIONS[to_or_from][category]['trans']
     elif category == 'low-carb':
-        load_transformations('healthy')
         trans_list = TRANSFORMATIONS[to_or_from]['healthy'][category]
     elif category == 'low-sodium':
-        load_transformations('healthy')
         trans_list = TRANSFORMATIONS[to_or_from]['healthy'][category]
     else:
         print "Category not found"
