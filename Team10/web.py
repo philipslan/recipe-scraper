5e9a04ea5d6dfec5a2abc5771f7cc367016a197c
hspindell <henryspindell@gmail.com>
2016-03-14 18:10:06 -0500

    moved web app and diagrams into Team10

--- Team10/web.py
+++ Team10/web.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template, jsonify, request
 import urllib
-import Team10.recipe_api
-from Team10.recipe_api import *
-import Team10.transformations.transformations
-from Team10.transformations.transformations import *
-import Team10.scraper
+import recipe_api
+from recipe_api import *
+import transformations.transformations
+from transformations.transformations import *
+import scraper
 from pprint import pprint
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ def index():
 @app.route('/_recipe_scraper/<path:url>')
 def get_recipe(url):
     output = parse_recipe_from_url(url)
-    recipe = Team10.scraper.get_recipe(url)
+    recipe = scraper.get_recipe(url)
     ingredients = recipe['ingredients']
     title = recipe['title']
 
@@ -32,8 +32,8 @@ def get_recipe(url):
 
 @app.route('/_transform/<path:url>/<to_or_from>/<category>')
 def transform(url, to_or_from, category):
-    recipe = Team10.scraper.get_recipe(url)
-    tr = Team10.transformations.transformations.transform(recipe, category, to_or_from)
+    recipe = scraper.get_recipe(url)
+    tr = transformations.transformations.transform(recipe, category, to_or_from)
     results = {}
     output = parse_recipe(tr)
 
