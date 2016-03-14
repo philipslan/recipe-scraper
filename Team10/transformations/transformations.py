10bbcf0ce6e268e3b52ec45e851e11e83231ec42
aforaleka <aforaleka@gmail.com>
2016-03-13 22:55:38 -0500

    healthy transformations done

--- Team10/transformations/transformations.py
+++ Team10/transformations/transformations.py
@@ -17,7 +17,7 @@ def load_transformations(category):
         TRANSFORMATIONS['from'][category] = {key: reverse(TRANSFORMATIONS['to'][category][key]) for key in TRANSFORMATIONS['to'][category]}
 
 def is_category(category, ingredients, title):
-    trans_list = []
+    trans_list = {}
     if category == 'vegetarian':
         load_transformations(category)
         trans_list = TRANSFORMATIONS['to'][category]['trans']
@@ -39,58 +39,63 @@ def is_category(category, ingredients, title):
     for i,x in enumerate(trans_list):
         if i == len(trans_list) - 1:
             regex += x
-        else:    
+        else:
             regex += x + "|"
     for ingredient in ingredients:
         if len(re.findall(regex,ingredient)):
-            return False    
+            return False
     return True
 
 # to_category is 'to' or 'from'
-def transform(recipe, category, to_or_from):
-    trans_list = []
-    if category is 'vegetarian' or category is 'vegan':
+def transform(recipe, category, to_or_from, typ=None):
+    trans_list = {}
+    if category == 'vegetarian' or category == 'vegan':
         trans_list = TRANSFORMATIONS[to_or_from][category]['trans']
         typ = 'veg'
-    elif category is 'low-carb' or category == 'low-sodium':
+    elif category == 'low-carb' or category == 'low-sodium':
         trans_list = TRANSFORMATIONS[to_or_from]['healthy'][category]
         typ = 'healthy'
     for key,value in recipe.iteritems():
         if key == "imageUrl":
             pass
         elif key == "title":
-            recipe[key] = transform_helper([value],trans_list, typ, to_or_from, key)[0]
+            recipe[key] = transform_helper([value], trans_list, typ, to_or_from, key, category)[0]
         else:
-            recipe[key] = transform_helper(value,trans_list, typ, to_or_from, key)
+            recipe[key] = transform_helper(value,trans_list, typ, to_or_from, key, category)
     return recipe
 
-def transform_helper(ingredients, transformations, typ,to_or_from, other):
+def transform_helper(ingredients, transformations, typ, to_or_from, other, category):
     final = []
     original_ingredients = ingredients[:]
     for i in xrange(len(ingredients)):
         for key,val in transformations.iteritems():
             replace = " or ".join(val) if len(val) > 1 else val[0]
-            ingredients[i] = re.sub(key,replace,ingredients[i])
+            ingredients[i] = re.sub(key,replace,ingredients[i].lower())
+
         if original_ingredients[i] != ingredients[i]:
             if typ == 'veg':
                 if to_or_from == 'to':
-                    ingredients[i] = re.sub("ground","crumbled",ingredients[i].lower())
+                    ingredients[i] = re.sub(r"ground",r"crumbled",ingredients[i].lower())
                 else:
-                    ingredients[i] = re.sub("crumbled","ground",ingredients[i].lower())
-            # if typ == 'healthy':
+                    ingredients[i] = re.sub(r"crumbled",r"ground",ingredients[i].lower())
 
         final.append(ingredients[i])
 
 
-
-    if typ == "veg":
-        if other == "title" and re.findall(r"vegetarian|vegan", final[0].lower()):
+    if typ is "veg":
+        if other is "title" and to_or_from is "from" and re.findall(r"vegetarian|vegan", final[0].lower()):
             final[0] = re.sub(r"vegetarian |vegan ", "", final[0].lower())
         elif [i.lower() for i in original_ingredients] == [i.lower() for i in ingredients]: # nothing changed
-            if other == "ingredients":
+            if other is "ingredients":
                 final.append('crumbled bacon')
-            if other == "directions":
+            if other is"directions":
                 final.append('Add crumbled bacon on top.')
+
+    # Change title to include what we transformed into
+    if other is "title" and to_or_from is "to" and category not in final[0].lower():
+        final[0] = category + " " + final[0]
+        final[0] = final[0].title()
+
 
     return final
 
--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 from Team10 import recipe_api
 
-recipe_api.autograder("http://allrecipes.com/recipe/24264/sloppy-joes-ii/?internalSource=recipe%20hub&referringId=471&referringContentType=recipe%20hub")
+recipe_api.autograder("http://allrecipes.com/recipe/90089/penne-with-chicken-and-asparagus/?internalSource=recipe%20hub&referringId=1788&referringContentType=recipe%20hub")
 
 
 
