77fda873f1344ffa22bea597e6df0778d8a158d6
hspindell <henryspindell@gmail.com>
2016-03-13 22:19:30 -0500

    added attribute labels and transformatino select

--- static/app.js
+++ static/app.js
@@ -31,53 +31,17 @@ recipe.controller('homeController', function ($scope,$http) {
             $scope.title = response['title'];
             $scope.imageUrl = response['imageUrl'];
             $scope.parsed = true;
- 
     });
   }
-  // $scope.change_year = function (year) {
-  //   $scope.category = false;
-  //   $scope.working = true;
-  //   $http({
-  //     url: $SCRIPT_ROOT + '/_get_year/' + year,
-  //     method: "GET"
-  //   }).success(function(response){
-  //     $scope.working = false;
-  //     $scope.category = response.result;
-  //   });
-  // }
-
-  // $scope.change_options = function (option) {
-  //   preprocess();
-  //   $http({
-  //     url: $SCRIPT_ROOT + '/_run_category',
-  //     method: "GET",
-  //     params: {
-  //       year: $scope.year,
-  //       category: option
-  //     }
-  //   }).success(function(response){
-  //     $scope.working = false;
-  //     switch (option) {
-  //       case "hosts":
-  //         $scope.name = $scope.year + " Golden Globe Hosts";
-  //         $scope.list_result = response.result;
-  //         break;
-  //       case "awards":
-  //         $scope.name = $scope.year + " Golden Globe Awards";
-  //         $scope.list_result = response.result;
-  //         break;
-  //       case "nominees":
-  //         $scope.name = $scope.year + " Golden Globe Nominees";
-  //         $scope.nom_keys = Object.keys(response.result);
-  //         $scope.nom_results = response.result;
-  //         break;
-  //       case "presenters":
-  //         $scope.name = $scope.year + " Golden Globe Presenters";
-  //         $scope.presenters = response.result;
-  //       case "winner":
-  //         $scope.name = $scope.year + " Golden Globe Winners";
-  //         $scope.winners = response.result;
-  //     }
-  //   });
-  // }
+  
+  $scope.getAttributes = function() {
+      var vegetarianAttr = $scope.vegetarian ? "vegetarian" : "non-vegetarian"
+      var veganAttr = $scope.vegan ? "vegan" : "non-vegan"
+      var lowCarbAttr = $scope.low_carb ? "low-carb" : "non-low-carb"
+      var lowSodiumAttr = $scope.low_sodium ? "low-sodium" : "non-low-sodium"
+      
+      return [vegetarianAttr,veganAttr,lowCarbAttr,lowSodiumAttr].join(", ")
+  }
+  
+  
 });
